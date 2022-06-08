131c184f291f95e8a85fa0ec7d6ec58788d6614e
tserg <8017125+tserg@users.noreply.github.com>
2022-06-08 13:46:10 +0800

    test: add setup tests; minor refactoring

--- tests/trove/test_trove.py
+++ tests/trove/test_trove.py
@@ -1,15 +1,12 @@
 import pytest
+
+from collections import namedtuple
+
 from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.testing.starknet import StarknetContract
 
 from utils import (
-    MAX_UINT256,
-    Uint256,
     compile_contract,
-    to_uint,
-    from_uint,
-    assert_event_emitted,
-    felt_to_str,
 )
 
 from random import uniform
@@ -23,23 +20,40 @@ FALSE = 0
 
 SCALE = 10**18
 
+GAGE0_STARTING_PRICE = 2000
+GAGE1_STARTING_PRICE = 500
+GAGE2_STARTING_PRICE = 1.25
+
+FEED_LEN = 20
+MAX_PRICE_CHANGE = 0.025
 SECONDS_PER_MINUTE = 60
+
+LIQUIDATION_THRESHOLD = 80
+
+#
+# Structs
+#
+
+Gage = namedtuple("Gage", ["total", "max"])
+Point = namedtuple("Point", ["price", "time"])
 
 # Utility functions
 
-def to_wad(n : float):
-    return int(n*SCALE)
 
-# Returns a price feed 
-def create_feed(starting_price : float, length : int, max_change : float) -> list[int]:
+def to_wad(n: float):
+    return int(n * SCALE)
+
+
+# Returns a price feed
+def create_feed(starting_price: float, length: int, max_change: float) -> list[int]:
     feed = []
 
     feed.append(starting_price)
     for i in range(1, length):
-        change = uniform(-max_change, max_change) # Returns the % change in price (in decimal form, meaning 1% = 0.01)
-        feed.append(feed[i-1]*(1 + change))
-    
-    return list(map(to_wad, feed)) #Scaling the feed before returning so it's ready to use in contracts
+        change = uniform(-max_change, max_change)  # Returns the % change in price (in decimal form, meaning 1% = 0.01)
+        feed.append(feed[i - 1] * (1 + change))
+
+    return list(map(to_wad, feed))  # Scaling the feed before returning so it's ready to use in contracts
 
 
 #
@@ -53,49 +67,96 @@ async def trove(starknet, users) -> StarknetContract:
     trove_owner = await users("trove owner")
     trove_contract = compile_contract("contracts/trove/trove.cairo")
 
-    trove = await starknet.deploy(
-        contract_def=trove_contract, 
-        constructor_calldata=[
-            trove_owner.address
-        ]
-    )
+    trove = await starknet.deploy(contract_def=trove_contract, constructor_calldata=[trove_owner.address])
 
-    return trove 
+    return trove
+
 
 # Same as above but also comes with ready-to-use gages and price feeds
-@pytest.fixture 
+@pytest.fixture
 async def trove_setup(users, trove) -> StarknetContract:
 
     trove_owner = await users("trove owner")
 
-    # Creating the gages
-    await trove_owner.send_tx(trove.contract_address, "add_gage", [to_wad(10_000)]) 
-    await trove_owner.send_tx(trove.contract_address, "add_gage", [to_wad(50_000)]) 
-    await trove_owner.send_tx(trove.contract_address, "add_gage", [to_wad(10_000_000)]) 
+    # Set liquidation threshold
+    await trove_owner.send_tx(trove.contract_address, "set_threshold", [to_wad(LIQUIDATION_THRESHOLD)])
 
-    feed_len = 20
+    # Creating the gages
+    await trove_owner.send_tx(trove.contract_address, "add_gage", [to_wad(10_000)])
+    await trove_owner.send_tx(trove.contract_address, "add_gage", [to_wad(50_000)])
+    await trove_owner.send_tx(trove.contract_address, "add_gage", [to_wad(10_000_000)])
+
     # Creating the price feeds
-    feed0 = create_feed(2000, feed_len, 0.025)
-    feed1 = create_feed(500, feed_len, 0.025)
-    feed2 = create_feed(1.25, feed_len, 0.025)
+    feed0 = create_feed(GAGE0_STARTING_PRICE, FEED_LEN, MAX_PRICE_CHANGE)
+    feed1 = create_feed(GAGE1_STARTING_PRICE, FEED_LEN, MAX_PRICE_CHANGE)
+    feed2 = create_feed(GAGE2_STARTING_PRICE, FEED_LEN, MAX_PRICE_CHANGE)
 
     # Putting the price feeds in the `series` storage variable
-    for i in range(feed_len):
-        trove_owner.send_tx(trove.contract_address, "advance", [0, feed0[i], i*30*SECONDS_PER_MINUTE])
-        trove_owner.send_tx(trove.contract_address, "advance", [1, feed1[i], i*30*SECONDS_PER_MINUTE])
-        trove_owner.send_tx(trove.contract_address, "advance", [2, feed2[i], i*30*SECONDS_PER_MINUTE])
-    
+    for i in range(FEED_LEN):
+        await trove_owner.send_tx(
+            trove.contract_address,
+            "advance",
+            [0, feed0[i], i * 30 * SECONDS_PER_MINUTE],
+        )
+        await trove_owner.send_tx(
+            trove.contract_address,
+            "advance",
+            [1, feed1[i], i * 30 * SECONDS_PER_MINUTE],
+        )
+        await trove_owner.send_tx(
+            trove.contract_address,
+            "advance",
+            [2, feed2[i], i * 30 * SECONDS_PER_MINUTE],
+        )
+
     return trove
+
 
 #
 # Tests
-# 
+#
+
+
+@pytest.mark.asyncio
+async def test_trove(trove_setup):
+    trove = trove_setup
+
+    # Check threshold
+    threshold = (await trove.get_threshold().invoke()).result.threshold
+    assert threshold == to_wad(LIQUIDATION_THRESHOLD)
+
+    # Check gage count
+    gage_count = (await trove.get_num_gages().invoke()).result.num
+    assert gage_count == 3
+
+    # Check gages
+    gage0 = (await trove.get_gages(0).invoke()).result.gage
+    assert gage0 == Gage(0, to_wad(10_000))
+
+    gage1 = (await trove.get_gages(1).invoke()).result.gage
+    assert gage1 == Gage(0, to_wad(50_000))
+
+    gage2 = (await trove.get_gages(2).invoke()).result.gage
+    assert gage2 == Gage(0, to_wad(10_000_000))
+
+    # Check price feeds
+    gage0_first_point = (await trove.get_series(0, 0).invoke()).result.point
+    assert gage0_first_point == Point(to_wad(GAGE0_STARTING_PRICE), 0)
+
+    gage0_last_point = (await trove.get_series(0, FEED_LEN - 1).invoke()).result.point
+    assert gage0_last_point.time == (FEED_LEN - 1) * 30 * SECONDS_PER_MINUTE
+
+    gage0_null_point = (await trove.get_series(0, FEED_LEN).invoke()).result.point
+    assert gage0_null_point == Point(0, 0)
+
+    gage0_series_len = (await trove.get_series_len(0).invoke()).result.len
+    assert gage0_series_len == 20
+
 
 @pytest.mark.asyncio
 async def test_auth(trove, users):
 
     trove_owner = await users("trove owner")
-
 
     #
     # Auth
@@ -113,7 +174,7 @@ async def test_auth(trove, users):
     c_authorized = (await trove.get_auth(c.address).invoke()).result.is_auth
     assert c_authorized == TRUE
 
-    #Revoking an address
+    # Revoking an address
     await b.send_tx(trove.contract_address, "revoke", [c.address])
     c_authorized = (await trove.get_auth(c.address).invoke()).result.is_auth
     assert c_authorized == FALSE
@@ -121,14 +182,3 @@ async def test_auth(trove, users):
     # Calling an authorized function with an unauthorized address - should fail
     with pytest.raises(StarkException):
         await c.send_tx(trove.contract_address, "revoke", [b.address])
-
-@pytest.mark.asyncio 
-async def test_trove(trove_setup, users):
-    trove = trove_setup
-
-    
-
-
-
-
-
