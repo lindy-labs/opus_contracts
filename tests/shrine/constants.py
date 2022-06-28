a50273ce1f8d2d59dce503c650f35f64a8df8dbe
tserg <8017125+tserg@users.noreply.github.com>
2022-06-28 11:36:49 +0800

    update tests

--- tests/shrine/constants.py
+++ tests/shrine/constants.py
@@ -5,14 +5,16 @@ from tests.utils import RAY_SCALE, WAD_SCALE
 # Shrine setup constants
 GAGES = [
     {
+        "address": 1234,
         "start_price": 2000,
         "ceiling": 10_000 * WAD_SCALE,
     },
     {
+        "address": 2345,
         "start_price": 500,
         "ceiling": 100_000 * WAD_SCALE,
     },
-    {"start_price": 1.25, "ceiling": 10_000_000 * WAD_SCALE},
+    {"address": 3456, "start_price": 1.25, "ceiling": 10_000_000 * WAD_SCALE},
 ]
 
 FEED_LEN = 20
