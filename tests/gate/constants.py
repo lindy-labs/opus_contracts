11d18cb9743fe5f10cbc8668e65db37c0ca8b933
tserg <8017125+tserg@users.noreply.github.com>
2022-06-29 17:49:41 +0800

    setup for deposit test

--- tests/gate/constants.py
+++ tests/gate/constants.py
@@ -1,5 +1,6 @@
 import decimal
 
-from tests.utils import to_ray
+from tests.utils import to_ray, to_wad
 
+FIRST_DEPOSIT_AMT = to_wad(50)
 TAX = to_ray(decimal.Decimal("0.05"))
--- tests/gate/test_gage_gate.py
+++ tests/gate/test_gage_gate.py
@@ -1,16 +1,53 @@
 import pytest
+from starkware.starknet.testing.objects import StarknetTransactionExecutionInfo
 
-from tests.gate.constants import TAX
-from tests.utils import TRUE, from_uint
+from tests.gate.constants import FIRST_DEPOSIT_AMT, TAX
+from tests.utils import MAX_UINT256, TRUE, from_uint, to_uint
+
+#
+# Fixtures
+#
+
+
+@pytest.fixture
+async def gate_deposit(users, gate_gage_rebasing, gage_rebasing) -> StarknetTransactionExecutionInfo:
+    gate = gate_gage_rebasing
+
+    shrine_user = await users("shrine user")
+    abbot = await users("abbot")
+
+    # Approve Gate to transfer tokens from user
+    await shrine_user.send_tx(gage_rebasing.contract_address, "approve", [gate.contract_address, *MAX_UINT256])
+
+    # Check user balance
+    # bal = (await gage_rebasing.balanceOf(shrine_user.address).invoke()).result.balance
+    # assert bal == to_uint(0)
+
+    # Call deposit
+    deposit = await abbot.send_tx(gate.contract_address, "deposit", [*(FIRST_DEPOSIT_AMT, 0), shrine_user.address])
+    return deposit
+
+
+#
+# Tests
+#
 
 
 @pytest.mark.asyncio
-async def test_gate_setup(gate_gage_rebasing, users):
+async def test_gate_setup(gate_gage_rebasing, gage_rebasing, users):
     gate = gate_gage_rebasing
 
     # Check system is live
     live = (await gate.get_live().invoke()).result.bool
     assert live == TRUE
+
+    # Check asset address
+    asset = (await gate.asset().invoke()).result.assetTokenAddress
+    assert asset == gage_rebasing.contract_address
+
+    # Check total assets
+    asset_bal = (await gate.totalAssets().invoke()).result.totalManagedAssets
+    assert asset_bal == to_uint(0)
 
     # Check Abbot address is authorized
     abbot = await users("abbot")
@@ -28,3 +65,14 @@ async def test_gate_setup(gate_gage_rebasing, users):
 
     # Check initial values
     assert from_uint((await gate.totalSupply().invoke()).result.totalSupply) == 0
+
+
+@pytest.mark.asyncio
+async def test_gate_deposit(users, gate_gage_rebasing, gage_rebasing, gate_deposit):
+    gate = gate_gage_rebasing
+
+    shrine_user = await users("shrine user")
+
+    # Check vault shares balance
+    total_shares = (await gate.totalSupply.invoke()).result.totalSupply
+    assert total_shares == FIRST_DEPOSIT_AMT
