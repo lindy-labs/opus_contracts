43f7d319912d200d38ef51df1adfabd238a0ce9a
tserg <8017125+tserg@users.noreply.github.com>
2022-06-29 22:31:17 +0800

    add events to tests

--- tests/gate/test_gage_gate.py
+++ tests/gate/test_gage_gate.py
@@ -2,7 +2,7 @@ import pytest
 from starkware.starknet.testing.objects import StarknetTransactionExecutionInfo
 
 from tests.gate.constants import FIRST_DEPOSIT_AMT, FIRST_REBASE_AMT, TAX
-from tests.utils import MAX_UINT256, TRUE, from_ray, from_uint, to_uint
+from tests.utils import MAX_UINT256, TRUE, assert_event_emitted, from_ray, from_uint
 
 #
 # Fixtures
@@ -56,8 +56,8 @@ async def test_gate_setup(gate_gage_rebasing, gage_rebasing, users):
     assert asset == gage_rebasing.contract_address
 
     # Check total assets
-    asset_bal = (await gate.totalAssets().invoke()).result.totalManagedAssets
-    assert asset_bal == to_uint(0)
+    asset_bal = from_uint((await gate.totalAssets().invoke()).result.totalManagedAssets)
+    assert asset_bal == 0
 
     # Check Abbot address is authorized
     abbot = await users("abbot")
@@ -81,19 +81,28 @@ async def test_gate_setup(gate_gage_rebasing, gage_rebasing, users):
 async def test_gate_deposit(users, gate_gage_rebasing, gage_rebasing, gate_deposit):
     gate = gate_gage_rebasing
 
+    abbot = await users("abbot")
     shrine_user = await users("shrine user")
 
     # Check vault underlying balance
-    total_bal = (await gage_rebasing.balanceOf(gate.contract_address).invoke()).result.balance
+    total_bal = from_uint((await gage_rebasing.balanceOf(gate.contract_address).invoke()).result.balance)
     total_assets = (await gate.totalAssets().invoke()).result.totalManagedAssets
-    assert total_bal == total_assets == to_uint(FIRST_DEPOSIT_AMT)
+    assert total_bal == from_uint(total_assets) == FIRST_DEPOSIT_AMT
+
+    underlying_bal = (await gate.get_last_underlying_balance().invoke()).result.wad
+    assert underlying_bal == total_bal
 
     # Check vault shares balance
-    total_shares = (await gate.totalSupply().invoke()).result.totalSupply
+    total_shares = from_uint((await gate.totalSupply().invoke()).result.totalSupply)
     user_shares = (await gate.balanceOf(shrine_user.address).invoke()).result.balance
-    assert total_shares == user_shares == to_uint(FIRST_DEPOSIT_AMT)
+    assert total_shares == from_uint(user_shares) == FIRST_DEPOSIT_AMT
 
-    # TODO Test events
+    assert_event_emitted(
+        gate_deposit,
+        gate.contract_address,
+        "Deposit",
+        [abbot.address, shrine_user.address, *total_assets, *user_shares],
+    )
 
 
 @pytest.mark.asyncio
@@ -101,14 +110,39 @@ async def test_gate_sync(users, gate_gage_rebasing, gage_rebasing, rebase):
     gate = gate_gage_rebasing
 
     abbot = await users("abbot")
+    taxman = await users("taxman")
+    shrine_user = await users("shrine user")
+
+    # Get balances before sync
+    before_taxman_bal = from_uint((await gage_rebasing.balanceOf(taxman.address).invoke()).result.balance)
+    rebased_bal = from_uint((await gage_rebasing.balanceOf(gate.contract_address).invoke()).result.balance)
 
     # Check gage token contract for rebased balance
-    rebased_bal = (await gage_rebasing.balanceOf(gate.contract_address).invoke()).result.balance
-    assert rebased_bal == to_uint(FIRST_DEPOSIT_AMT + FIRST_REBASE_AMT)
+    assert rebased_bal == FIRST_DEPOSIT_AMT + FIRST_REBASE_AMT
+
+    # Fetch last underlying balance
+    before_underlying_bal = (await gate.get_last_underlying_balance().invoke()).result.wad
 
     # Update Gate's balance and charge tax
     sync = await abbot.send_tx(gate.contract_address, "sync", [])
-    after_bal = (await gate.totalAssets().invoke()).result.totalManagedAssets
-    assert after_bal.low == rebased_bal.low - (from_ray(TAX) * FIRST_REBASE_AMT)
 
-    # TODO Test events
+    # Check Gate's managed assets and balance
+    after_gate_bal = from_uint((await gate.totalAssets().invoke()).result.totalManagedAssets)
+    after_underlying_bal = (await gate.get_last_underlying_balance().invoke()).result.wad
+
+    tax = int(from_ray(TAX) * FIRST_REBASE_AMT)
+    increment = FIRST_REBASE_AMT - tax
+
+    assert after_gate_bal == after_underlying_bal == rebased_bal - tax == before_underlying_bal + increment
+
+    # Check that user's redeemable balance has increased
+    user_shares_uint = (await gate.balanceOf(shrine_user.address).invoke()).result.balance
+    user_underlying = from_uint((await gate.previewRedeem(user_shares_uint).invoke()).result.assets)
+    assert user_underlying == after_gate_bal
+
+    # Check event emitted
+    assert_event_emitted(sync, gate.contract_address, "Sync", [before_underlying_bal, after_underlying_bal, tax])
+
+    # Check taxman has received tax
+    after_taxman_bal = from_uint((await gage_rebasing.balanceOf(taxman.address).invoke()).result.balance)
+    assert after_taxman_bal == before_taxman_bal + tax
