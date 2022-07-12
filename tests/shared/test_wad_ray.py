975065d3680bc37c72b0ea35067b7f2588f2eba9
tserg <8017125+tserg@users.noreply.github.com>
2022-07-12 15:34:02 +0800

    add tests for wsigned_div

--- tests/shared/test_wad_ray.py
+++ tests/shared/test_wad_ray.py
@@ -2,18 +2,19 @@ import math
 import operator
 
 import pytest
-from hypothesis import example, given, settings
+from hypothesis import assume, example, given, settings
 from hypothesis import strategies as st
 from starkware.starknet.testing.starknet import StarknetContract
 from starkware.starkware_utils.error_handling import StarkException
 
-from tests.utils import RANGE_CHECK_BOUND, WAD_SCALE, compile_contract, signed_int_to_felt, to_wad
+from tests.utils import PRIME, RANGE_CHECK_BOUND, WAD_SCALE, compile_contract, signed_int_to_felt, to_wad
 
 BOUND = 2**125
 
 
 st_int = st.integers(min_value=-(2**250), max_value=2**250)
 st_int128 = st.integers(min_value=-(2**128), max_value=2**128)
+st_uint128 = st.integers(min_value=1, max_value=2**128)
 st_uint = st.integers(min_value=0, max_value=2 * 200)
 
 
@@ -159,23 +160,38 @@ async def test_add_sub(wad_ray, left, right, fn, op):
     "fn,op",
     [
         ("test_wmul", operator.mul),
+        ("test_wsigned_div", operator.floordiv),
     ],
 )
 @pytest.mark.asyncio
 async def test_checked(wad_ray, left, right, fn, op):
+    # skip right = 0
+    assume(right != 0)
+
     left_input_val = signed_int_to_felt(left)
     right_input_val = signed_int_to_felt(right)
+
+    if fn.endswith(("wsigned_div",)):
+        sign = -1 if right < 0 else 1
+        # Convert right to absolute value before converting it to felt
+        right = abs(right)
+        # `signed_div_rem` assumes 0 < right <= PRIME / RANGE_CHECK_BOUND
+        assume(right <= PRIME // RANGE_CHECK_BOUND)
+        # Scale left by wad after converting it to felt for computation of python value
+        left *= WAD_SCALE
 
     expected_py = op(left, right)
 
     if fn.endswith(("wmul",)):
         expected_py //= WAD_SCALE
+    elif fn.endswith(("wsigned_div",)):
+        expected_py *= sign
 
     expected_cairo = signed_int_to_felt(expected_py)
 
     method = wad_ray.get_contract_function(fn)
 
-    if expected_py < -BOUND or expected_py >= BOUND:
+    if abs(expected_py) > BOUND or expected_cairo == PRIME:
         with pytest.raises(StarkException):
             await method(left_input_val, right_input_val).invoke()
 
