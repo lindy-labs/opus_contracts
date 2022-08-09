d53c7b76a76463e92d4a09468f4e2db4c21a539a
bllu404 <96asdffdsa69@protonmail.com>
2022-08-09 17:24:58 +0300

    dev(yin-gate): transferFrom test

--- tests/yin/test_yin.py
+++ tests/yin/test_yin.py
@@ -2,8 +2,10 @@ import pytest
 from starkware.starknet.testing.starknet import StarknetContract
 from starkware.starkware_utils.error_handling import StarkException
 
-from tests.shrine.constants import FORGE_AMT, USER_1, USER_2
+from tests.shrine.constants import FORGE_AMT, USER_1, USER_2, USER_3
 from tests.utils import assert_event_emitted, compile_contract, str_to_felt
+
+INFINITE_ALLOWANCE = 2**125
 
 
 @pytest.fixture
@@ -25,7 +27,7 @@ async def yin(yin_deploy, shrine, users) -> StarknetContract:
 
 
 @pytest.mark.asyncio
-async def test_yin_transfer_pass(shrine, shrine_forge, yin):
+async def test_yin_transfer_pass(shrine_forge, yin):
 
     # Checking USER_1's and USER_2's initial balance
     u1_bal = (await yin.balanceOf(USER_1).invoke()).result.balance
@@ -36,6 +38,8 @@ async def test_yin_transfer_pass(shrine, shrine_forge, yin):
 
     # Transferring all of USER_1's balance to USER_2
     transfer_tx = await yin.transfer(USER_2, FORGE_AMT).invoke(caller_address=USER_1)
+
+    assert transfer_tx.result.success == 1
 
     u1_new_bal = (await yin.balanceOf(USER_1).invoke()).result.balance
     assert u1_new_bal == 0
@@ -57,6 +61,38 @@ async def test_yin_transfer_fail(shrine_forge, yin):
 
     # Attempting to transfer 0 yin when USER_2 owns nothing - should pass
     await yin.transfer(USER_1, 0).invoke(caller_address=USER_2)
+
+
+@pytest.mark.asyncio
+async def test_yin_transfer_from_pass(shrine_forge, yin):
+
+    # USER_1 approves USER_2
+    approve_tx = await yin.approve(USER_2, FORGE_AMT).invoke(caller_address=USER_1)
+    assert approve_tx.result.success == 1
+
+    # Checking USER_2's allowance for USER_1
+    allowance = (await yin.allowance(USER_1, USER_2).invoke()).result.remaining
+    assert allowance == FORGE_AMT
+
+    # USER_2 transfers all of USER_1's funds to USER_3
+    await yin.transferFrom(USER_1, USER_3, FORGE_AMT).invoke(caller_address=USER_2)
+
+    # Checking balances
+    u1_bal = (await yin.balanceOf(USER_1).invoke()).result.balance
+    assert u1_bal == 0
+
+    u3_bal = (await yin.balanceOf(USER_3).invoke()).result.balance
+    assert u3_bal == FORGE_AMT
+
+    # Checking USER_2's allowance
+    u2_allowance = (await yin.allowance(USER_1, USER_2).invoke()).result.remaining
+    assert u2_allowance == 0
+
+    # infinite allowance test
+    await yin.approve(USER_2, INFINITE_ALLOWANCE).invoke(caller_address=USER_3)
+    await yin.transferFrom(USER_3, USER_1, FORGE_AMT).invoke(caller_address=USER_2)
+    u2_allowance = (await yin.allowance(USER_3, USER_2).invoke()).result.remaining
+    assert u2_allowance == INFINITE_ALLOWANCE
 
 
 @pytest.mark.asyncio
