d4fb55cc4b7409a74d0fd56be13bf08185b2ad5f
bllu404 <96asdffdsa69@protonmail.com>
2022-08-09 17:09:38 +0300

    dev(yin-gate): invalid inputs test and failed transfer test

--- tests/yin/test_yin.py
+++ tests/yin/test_yin.py
@@ -1,5 +1,6 @@
 import pytest
 from starkware.starknet.testing.starknet import StarknetContract
+from starkware.starkware_utils.error_handling import StarkException
 
 from tests.shrine.constants import FORGE_AMT, USER_1, USER_2
 from tests.utils import assert_event_emitted, compile_contract, str_to_felt
@@ -42,3 +43,33 @@ async def test_yin_transfer_pass(shrine, shrine_forge, yin):
     assert u2_new_bal == FORGE_AMT
 
     assert_event_emitted(transfer_tx, yin.contract_address, "Transfer", [USER_1, USER_2, FORGE_AMT])
+
+
+@pytest.mark.asyncio
+async def test_yin_transfer_fail(shrine_forge, yin):
+    # Attempting to transfer more yin than USER_1 owns
+    with pytest.raises(StarkException, match="Shrine: transfer amount exceeds yin balance"):
+        await yin.transfer(USER_2, FORGE_AMT + 1).invoke(caller_address=USER_1)
+
+    # Attempting to transfer any amount of yin when USER_2 owns nothing
+    with pytest.raises(StarkException, match="Shrine: transfer amount exceeds yin balance"):
+        await yin.transfer(USER_1, 1).invoke(caller_address=USER_2)
+
+    # Attempting to transfer 0 yin when USER_2 owns nothing - should pass
+    await yin.transfer(USER_1, 0).invoke(caller_address=USER_2)
+
+
+@pytest.mark.asyncio
+async def test_yin_invalid_inputs(yin):
+
+    with pytest.raises(StarkException, match=r"Yin: amount is not in the valid range \[0, 2\*\*125\]"):
+        await yin.transfer(USER_2, -1).invoke(caller_address=USER_1)
+
+    with pytest.raises(StarkException, match=r"Yin: amount is not in the valid range \[0, 2\*\*125\]"):
+        await yin.transfer(USER_2, 2**125 + 1).invoke(caller_address=USER_1)
+
+    with pytest.raises(StarkException, match=r"Yin: amount is not in the valid range \[0, 2\*\*125\]"):
+        await yin.approve(USER_2, -1).invoke(caller_address=USER_1)
+
+    with pytest.raises(StarkException, match=r"Yin: amount is not in the valid range \[0, 2\*\*125\]"):
+        await yin.transfer(USER_2, 2**125 + 1).invoke(caller_address=USER_1)
