f0f1fc5424cff3c43da25cabba8e2196e9ed8531
Milan Cermak <milan.cermak@gmail.com>
2022-08-23 13:42:21 +0200

    chore: removing use of PRIME in favor of CAIRO_PRIME

--- tests/utils.py
+++ tests/utils.py
@@ -15,9 +15,7 @@ from starkware.starknet.services.api.feeder_gateway.response_objects import Func
 from starkware.starknet.testing.objects import StarknetTransactionExecutionInfo
 from starkware.starknet.testing.starknet import Starknet, StarknetContract
 
-PRIME = 2**251 + 17 * 2**192 + 1
 RANGE_CHECK_BOUND = 2**128
-
 MAX_UINT256 = (2**128 - 1, 2**128 - 1)
 ZERO_ADDRESS = 0
 TRUE = 1
@@ -78,10 +76,10 @@ def as_address(value: Addressable) -> int:
 
 
 def signed_int_to_felt(a: int) -> int:
-    """Takes in integer value, returns input if positive, otherwise return PRIME + input"""
+    """Takes in integer value, returns input if positive, otherwise return CAIRO_PRIME + input"""
     if a >= 0:
         return a
-    return PRIME + a
+    return CAIRO_PRIME + a
 
 
 def felt_to_str(felt: int) -> str:
