66bdf2c3aadfecc03b1e6ac72a5625e963fea49c
exp_table <exp.table@protonmail.com>
2022-09-05 15:57:41 +0200

    Passing providing test

--- tests/stability_pool/test_stability_pool.py
+++ tests/stability_pool/test_stability_pool.py
@@ -6,16 +6,8 @@ from tests.purger.test_purger import purger, advance_yang_prices_by_percentage, 
 from tests.purger.constants import DEBT_CEILING_WAD
 
 from tests.utils import (
-    SHRINE_OWNER,
-    TROVE1_OWNER,
-    TROVE_1,
-    TIME_INTERVAL,
+    AURA_USER,
     compile_contract,
-    str_to_felt,
-    to_wad,
-    create_feed,
-    from_wad,
-    set_block_timestamp
 )
 from tests.shrine.constants import FEED_LEN, MAX_PRICE_CHANGE, MULTIPLIER_FEED
 from tests.roles import ShrineRoles
@@ -30,12 +22,16 @@ async def deployed_pool(request, shrine, yin, purger, starknet):
     #await shrine.forge(TROVE1_OWNER, TROVE_1, FORGE_AMT_WAD).invoke(caller_address=SHRINE_OWNER)
     return pool
 
-@pytest.mark.usefixtures("aura_user_with_first_trove")
+@pytest.mark.usefixtures(
+    "abbot_with_yangs",
+    "funded_aura_user",
+    "aura_user_with_first_trove"
+)
 @pytest.mark.asyncio
 async def test_provide(deployed_pool, shrine, yin, aura_user_with_first_trove):
     pool = deployed_pool
     amount = aura_user_with_first_trove
-    await yin.approve(pool.contract_address, amount).invoke(caller_address=TROVE1_OWNER)
-    await shrine.provide(amount).invoke(caller_address=TROVE1_OWNER)
-    deposit = (await pool.get_provider_owed_yin(TROVE1_OWNER)).result.yin
+    await yin.approve(pool.contract_address, amount).execute(caller_address=AURA_USER)
+    await pool.provide(amount).execute(caller_address=AURA_USER)
+    deposit = (await pool.get_provider_owed_yin(AURA_USER).execute()).result.yin
     assert deposit == amount
