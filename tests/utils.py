adb2d37c164b58f8706c927f98d83291a8169e9a
tserg <8017125+tserg@users.noreply.github.com>
2022-12-01 17:28:47 +0800

    test: reorder utils

--- tests/utils.py
+++ tests/utils.py
@@ -59,10 +59,15 @@ Addressable = Union[int, StarknetContract]
 Calldata = list[int]  # payload arguments sent with a function call
 Call = tuple[Addressable, str, Calldata]  # receiver address, selector (still as string) and payload
 
+
+def custom_error_margin(negative_exp: int) -> Decimal:
+    return Decimal(f"1E-{negative_exp}")
+
+
 # Default error margin for fixed point calculations
-ERROR_MARGIN = Decimal("1E-10")
-WAD_ERROR_MARGIN = Decimal("1E-18")
-WBTC_ERROR_MARGIN = Decimal("1E-8")
+ERROR_MARGIN = custom_error_margin(10)
+WAD_ERROR_MARGIN = custom_error_margin(WAD_DECIMALS)
+WBTC_ERROR_MARGIN = custom_error_margin(8)
 
 seed(420)
 
@@ -272,10 +277,6 @@ def from_ray(n: int) -> Decimal:
 
 def assert_equalish(a: Decimal, b: Decimal, error=ERROR_MARGIN):
     assert abs(a - b) <= error
-
-
-def custom_error_margin(negative_exp: int) -> Decimal:
-    return Decimal(f"1E-{negative_exp}")
 
 
 #
